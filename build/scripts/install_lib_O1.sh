98cf70b949433e2696d1359d30dc61dc768835f1
akmal <akmalns@gmail.com>
2023-12-26 05:14:49 +0000

    joe fix o1 enabled

--- build/scripts/install_lib_O1.sh
+++ build/scripts/install_lib_O1.sh
@@ -129,7 +129,7 @@ install_netconf_lib() {
 
    # libyang
    cd $NETCONF_PATH && \
-      git clone -b v1.0.184 --depth 1 https://github.com/CESNET/libyang.git && \
+      git clone -b v1.0.240 --depth 1 https://github.com/CESNET/libyang.git && \
       cd libyang && mkdir build && cd build && \
       cmake -DGEN_LANGUAGE_BINDINGS=ON -DGEN_PYTHON_BINDINGS=OFF \
             -DCMAKE_BUILD_TYPE:String="Debug" -DENABLE_BUILD_TESTS=OFF .. && \
@@ -141,12 +141,12 @@ install_netconf_lib() {
 
    # sysrepo
    cd $NETCONF_PATH && \
-      git clone -b v1.4.70 --depth 1  https://github.com/sysrepo/sysrepo.git && \
+      git clone -b v1.4.140 --depth 1 https://github.com/sysrepo/sysrepo.git && \
       cd sysrepo && sed -i -e 's/2000/30000/g;s/5000/30000/g' src/common.h.in && \
       mkdir build && cd build && \
       cmake -DGEN_LANGUAGE_BINDINGS=ON -DGEN_PYTHON_BINDINGS=OFF \
             -DCMAKE_BUILD_TYPE:String="Debug" -DENABLE_TESTS=OFF \
-	    -DREPOSITORY_LOC:PATH=/etc/sysrepo .. && \
+        -DREPOSITORY_LOC:PATH=/etc/sysrepo .. && \
       make -j2 && \
       $SUDO make install && $SUDO make sr_clean && \
       $SUDO ldconfig
@@ -166,8 +166,8 @@ install_netconf_lib() {
 
    # netopeer2
    cd $NETCONF_PATH && \
-      git clone -b v1.1.53 --depth 1 https://github.com/CESNET/Netopeer2.git && \
-      cd Netopeer2 && mkdir build && cd build && \
+      git clone -b libyang1 --depth 1 https://git-ntustoran.ddns.net/oran/netopeer2.git && \
+      cd netopeer2 && mkdir build && cd build && \
       cmake -DCMAKE_BUILD_TYPE:String="Debug" -DNP2SRV_DATA_CHANGE_TIMEOUT=30000 \
             -DNP2SRV_DATA_CHANGE_WAIT=OFF .. && \
       make -j2 && \
